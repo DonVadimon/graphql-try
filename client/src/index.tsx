b18484be3d9e31a20e2c2df883431a761ce3a93f
Vadim Khizhnyakov <provcydia@gmail.com>
2021-08-28 03:00:45 +0300

    netlify connention

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,7 +7,7 @@ import { Global } from './global.styles';
 import reportWebVitals from './reportWebVitals';
 
 const client = new ApolloClient({
-    uri: 'http://localhost:8000/graphql',
+    uri: process.env.IS_REMOTE ? 'https://graphql-try-server.herokuapp.com/graphql' : 'http://localhost:8000/graphql',
     cache: new InMemoryCache(),
 });
 
